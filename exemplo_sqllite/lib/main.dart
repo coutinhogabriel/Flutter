9db34eadaeb50d3f5414c25ed7830c0a14bed1bf
coutinhogabriel <gabriel.coutin@hotmail.com>
2024-04-02 22:01:39 -0300

    02/04/24

---
+++ exemplo_sqllite/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'view.dart';
+
+void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Exemplo SQLLite",
+    theme: ThemeData(
+      primarySwatch: Colors.blue
+    ),
+    home: HomePage(),
+    );
+    
+  }
+}
+
+
